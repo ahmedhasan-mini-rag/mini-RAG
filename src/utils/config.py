c0162db284db9eea0d69c16a720eacf440c3ddd1
Ahmed Hasan <ahmed.hassanen110@gmail.com>
2026-07-19 01:09:09 +0300

    add GOOGLE_API_KEY to environment configuration

--- src/utils/config.py
+++ src/utils/config.py
@@ -45,6 +45,7 @@ class Settings(BaseSettings):
     OPENAI_API_KEY: str = None
     OPENAI_BASE_URL: str = None
     COHERE_API_KEY: str = None
+    GOOGLE_API_KEY: str = None
 
     CHAT_MODEL_ID: str
     EMBEDDING_MODEL_ID: str
